79d0e1f3e98dbd35af02374383de511016676cd0
AnastasijaBalaeva <balaeva9anastasija@gmail.com>
2021-12-06 21:11:51 +0300

    work home 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const table = document.getElementById('myTable');
+	    table.addEventListener('click', (event) => {
+        if ((event.target.closest('tr') || event.target.tagName === 'tr') && !(event.target.parentElement.id === 'header' || event.target.id === 'header')) {
+            deleteRow(table, event.target);
+        }
+    });
 
 function decision() {
-	const table = document.getElementById('myTable');
-	document.getElementById('btnDec').addEventListener('click', function(event) {
+	
 	a = parseFloat(document.getElementById("inpA").value);
 	b = parseFloat(document.getElementById("inpB").value);
 	c = parseFloat(document.getElementById("inpC").value);
@@ -11,7 +16,7 @@ function decision() {
 
 	if (isNaN(a) || isNaN(b) || isNaN(c)) {
 		document.getElementById("inpDec").value = "Параметры не являются числами! ";
-		let str =  document.getElementById("inpA").value + ' x ^2 + ' + document.getElementById("inpB").value +' x + '+ document.getElementById("inpC").value +' = 0';
+		let str =  document.getElementById("inpA").value + ' x^2 + ' + document.getElementById("inpB").value +' x + '+ document.getElementById("inpC").value +' = 0';
 		
 		rez = "Параметры не являются числами! ";
 		addRow(str, rez);
@@ -47,54 +52,9 @@ function decision() {
 	
 	}
 
-	});
-	
-	table.addEventListener('click', (event) => {
-        if ((event.target.closest('tr') || event.target.tagName === 'tr') && !(event.target.parentElement.id === 'header' || event.target.id === 'header')) {
-            deleteRow( event.target);
-        }
-    });
-
-
-	/*function myFunction(val, rez) {
-		
-	
-			
-		let x = document.getElementById('myTable').insertRow(val);
-		if (val%3==1) {
-			let y = x.insertCell(0);
-			let z = x.insertCell(1);
-			let r = x.insertCell(2);
-			x.className = 'colour1';
-			y.className = 'text4';
-			y.innerHTML = val;
-			z.innerHTML = rez ;
-			r.innerHTML = '<td><button class="deleteRow"  onclick="rowDelete();">Remove</button></td>';
-		}
-		else if (val%3==2) {
-			let y = x.insertCell(0);
-			let z = x.insertCell(1);
-			let r = x.insertCell(2);
-			x.className = 'colour2';
-			y.className = 'text4';
-			y.innerHTML = val;
-			z.innerHTML = rez ;
-			r.innerHTML = '<td><button class="deleteRow"  onclick="rowDelete();">Remove</button></td>';
-		}
-		else {
-			let y = x.insertCell(0);
-			let z = x.insertCell(1);
-			let r = x.insertCell(2);
-			x.className = 'colour3';
-			y.className = 'text4';
-			y.innerHTML = val;
-			z.innerHTML = rez;	
-			r.innerHTML = '<td><button class="deleteRow"  onclick="rowDelete();">Remove</button></td>';
-		}
-
-	}*/
 
 }
+
 
 function addRow(equations, result) {
 	const table = document.getElementById('myTable');
@@ -134,14 +94,15 @@ function addRow(equations, result) {
 	
 	    if (table.classList.contains('_empty')) {
         table.classList.remove('_empty');
-        const emptyContainer = document.getElementById('empty');
-        emptyContainer.classList.remove('_isEmpty');
+       
     }
 }
 
 
-function deleteRow(target) {
-    const table = document.getElementById('myTable');
+function deleteRow(table, target) {
+
+	
+    
 	if (target.closest('tr')) {
         target.parentElement.remove();
     } else if (target.tagName === 'tr') {
@@ -157,29 +118,7 @@ function deleteRow(target) {
         })
     } else {
         table.classList.add('_empty');
-        const emptyContainer = document.getElementById('empty');
-        emptyContainer.classList.add('_isEmpty');
+       
     }
 }
 
-
-
-/*function rowCount() {
-	let procRows = document.getElementById('myTable').rows.length;	
-
-	for (let i = 1; i < procRows; i++) {
-		document.getElementById('myTable').rows[i].cells[0].innerText = i;	  
-		let xRow = document.getElementById('myTable').rows[i]; 
-		if (i%3==1) {
-			xRow.className = 'colour1';
-		}
-		else if (i%3==2) {
-			xRow.className = 'colour2';
-		}
-		else {
-			xRow.className = 'colour3';
-		}  
-	  
-	}
-
-}*/
